64f9262f5bc740cf5df0e59f1a3465ff5520b822
Alex Buijs <contact@alexbuijsweb.nl>
2014-11-09 13:20:59 +0100

    update rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Alexbuijsweb::Application.config.session_store :cookie_store, key: '_alexbuijsweb_session'
+Rails.application.config.session_store :cookie_store, key: '_alexbuijsweb_session'
